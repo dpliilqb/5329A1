218623ee7dce05d96801597a3b09f79ece2150f4
Github <1300327964@qq.com>
2024-04-04 22:37:23 +1100

    data insight

--- main.py
+++ main.py
@@ -11,4 +11,11 @@ if __name__ == "__main__":
     train_data_array = np.load('Dataset/train_data.npy')
     train_label_array = np.load('Dataset/train_label.npy')
     test_data = pd.DataFrame(test_data_array)
-    print(test_data.head(3))
+    test_label = pd.DataFrame(test_label_array)
+    train_label = pd.DataFrame(train_label_array)
+    train_data = pd.DataFrame(train_data_array)
+    print("train data shape: ", train_data.shape)
+    print("test data shape: ", test_data.shape)
+    print("test_data:\n",train_data.head(3))
+    print("test_label:\n",train_label.head(3))
+
