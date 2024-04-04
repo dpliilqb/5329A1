008067641bafa13dcd7ef5432c557062685cbae8
Github <1300327964@qq.com>
2024-04-04 17:35:18 +1100

    test 1

--- Modules.py
+++ Modules.py
@@ -1,1 +1,4 @@
+def hi():
+    print("suc")
+
 print("module")
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
-
+import Modules as md
 def test():
     print("yes")
 
 
 if __name__ == "__main__":
     test()
+    md.hi()
     print("hi")
